a5641a2cdd4f4512b17262658a3ce2116e15e9de
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-25 17:57:00 -0300

    Created Owner and Profile Repositories

---
+++ src/main/java/com/brendamareco/portfolio/repositories/OwnerRepository.java
@@ -0,0 +1,9 @@
+package com.brendamareco.portfolio.repositories;
+
+import com.brendamareco.portfolio.entities.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+}

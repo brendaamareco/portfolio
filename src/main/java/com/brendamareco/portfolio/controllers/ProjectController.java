1dd0179c82d17b43b32017dbcb7abbf8204d9b46
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-30 16:16:23 -0300

    Added crossOrigin annotation

--- src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
@@ -1,8 +1,14 @@
 package com.brendamareco.portfolio.controllers;
 
+import com.brendamareco.portfolio.entities.Project;
 import com.brendamareco.portfolio.repositories.ProjectRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 public class ProjectController
 {
@@ -13,5 +19,10 @@ public class ProjectController
         this.projectRepository = projectRepository;
     }
 
+    @GetMapping("/api/projects")
+    public List<Project> getAll()
+    {
+        return this.projectRepository.findAll();
+    }
 
 }

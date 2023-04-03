71a8d5e27ae63ada414a040e421a9564fd5a4069
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-03 15:47:09 -0300

    Added Post mapping to project controller

--- src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
@@ -2,9 +2,8 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.Project;
 import com.brendamareco.portfolio.repositories.ProjectRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +24,24 @@ public class ProjectController
         return this.projectRepository.findAll();
     }
 
+    @PostMapping("/api/projects")
+    public ResponseEntity<Project> add(@RequestBody Project project)
+    {
+        if ( this.invalidProject(project) )
+            return ResponseEntity.badRequest().build();
+        else
+            return ResponseEntity.ok(this.projectRepository.save(project));
+    }
+
+    private boolean invalidProject(Project project)
+    {
+        boolean invalidId = project.getId() != null;
+
+        boolean invalidTitle = project.getTitle() == null
+                || project.getTitle().length() > 32;
+
+        boolean invalidOwner = project.getOwner() == null;
+
+        return invalidId || invalidTitle || invalidOwner;
+    }
 }

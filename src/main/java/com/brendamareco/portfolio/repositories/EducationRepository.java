9a2cefaab0a3d23c4bfbaa48accf885123153946
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-04 17:12:47 -0300

    Created education repository

---
+++ src/main/java/com/brendamareco/portfolio/repositories/EducationRepository.java
@@ -0,0 +1,8 @@
+package com.brendamareco.portfolio.repositories;
+
+import com.brendamareco.portfolio.entities.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EducationRepository extends JpaRepository<Education,Long> { }

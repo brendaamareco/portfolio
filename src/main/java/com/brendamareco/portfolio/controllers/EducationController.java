cf41dde9851bbbf2a3e71b624a320e6e94d57dd9
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-04 17:17:32 -0300

    Created education controller with get method

---
+++ src/main/java/com/brendamareco/portfolio/controllers/EducationController.java
@@ -0,0 +1,27 @@
+package com.brendamareco.portfolio.controllers;
+
+import com.brendamareco.portfolio.entities.Education;
+import com.brendamareco.portfolio.repositories.EducationRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/")
+public class EducationController
+{
+    private final String ROOT_URL = "http://localhost:8080/api/";
+    private EducationRepository educationRepository;
+
+    public EducationController(EducationRepository educationRepository) {
+        this.educationRepository = educationRepository;
+    }
+
+    @GetMapping(ROOT_URL + "education")
+    public List<Education> getEducationList()
+    {
+        return this.educationRepository.findAll();
+    }
+}

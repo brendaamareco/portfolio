99b9c8a1a0f6270806facb6f72fa15f9549ef9b4
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-07 14:01:51 -0300

    Created getAll method in ExperienceController

--- src/main/java/com/brendamareco/portfolio/controllers/ExperienceController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/ExperienceController.java
@@ -1,8 +1,12 @@
 package com.brendamareco.portfolio.controllers;
 
+import com.brendamareco.portfolio.entities.Experience;
 import com.brendamareco.portfolio.repositories.ExperienceRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -13,5 +17,11 @@ public class ExperienceController
 
     public ExperienceController(ExperienceRepository experienceRepository) {
         this.experienceRepository = experienceRepository;
+    }
+
+    @GetMapping(ROOT_URL + "experience")
+    public List<Experience> getAll()
+    {
+        return this.experienceRepository.findAll();
     }
 }

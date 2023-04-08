e54c799c9f4a9c58cad84e6f6095e94df7747f45
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-08 15:32:50 -0300

    Created add method in ExperienceController

--- src/main/java/com/brendamareco/portfolio/controllers/ExperienceController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/ExperienceController.java
@@ -2,9 +2,8 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.Experience;
 import com.brendamareco.portfolio.repositories.ExperienceRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,28 @@ public class ExperienceController
     public List<Experience> getAll()
     {
         return this.experienceRepository.findAll();
+    }
+
+    @PostMapping(ROOT_URL + "experience")
+    public ResponseEntity<Experience> add(@RequestBody Experience experience)
+    {
+        boolean validId = experience.getId() == null;
+
+        if (!validId || !validExperience(experience))
+            return ResponseEntity.badRequest().build();
+
+        else
+        {
+            this.experienceRepository.save(experience);
+            return ResponseEntity.ok(experience);
+        }
+    }
+
+    private boolean validExperience(Experience experience)
+    {
+        return experience.getCompanyName() != null
+                && experience.getTitle() != null
+                && experience.getStartDate() != null
+                && experience.getEndDate() != null;
     }
 }

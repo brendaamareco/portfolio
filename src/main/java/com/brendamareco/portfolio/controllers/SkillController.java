3b8a95c9e644699e93602b5d434e5d3da346e5f6
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-10 18:03:09 -0300

    Created GetAll method in Skill Controller

--- src/main/java/com/brendamareco/portfolio/controllers/SkillController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/SkillController.java
@@ -1,9 +1,13 @@
 package com.brendamareco.portfolio.controllers;
 
+import com.brendamareco.portfolio.entities.Skill;
 import com.brendamareco.portfolio.repositories.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -11,4 +15,11 @@ public class SkillController
 {
     @Autowired
     private SkillRepository skillRepository;
+    private final String ROOT_URL = "/api/";
+
+    @GetMapping(ROOT_URL + "skill")
+    public List<Skill> getAll()
+    {
+        return this.skillRepository.findAll();
+    }
 }

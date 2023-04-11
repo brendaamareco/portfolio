5e11949e01c7941a72fc8f2eca2f2defb7606a95
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-11 15:26:25 -0300

    SoftSkillService extends DefaultService

--- src/main/java/com/brendamareco/portfolio/services/SoftSkillService.java
+++ src/main/java/com/brendamareco/portfolio/services/SoftSkillService.java
@@ -1,25 +1,10 @@
 package com.brendamareco.portfolio.services;
 
 import com.brendamareco.portfolio.entities.SoftSkill;
+import com.brendamareco.portfolio.interfaces.DefaultService;
 import com.brendamareco.portfolio.repositories.SoftSkillRepository;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class SoftSkillService
-{
-    @Autowired SoftSkillRepository repository;
-
-    public List<SoftSkill> getAll()
-    {
-        return this.repository.findAll();
-    }
-    public SoftSkill add(SoftSkill softSkill)
-    {
-        softSkill.setId(null);
-        return this.repository.save(softSkill);
-    }
-}
+public class SoftSkillService extends DefaultService<SoftSkill, Long, SoftSkillRepository>
+{ }

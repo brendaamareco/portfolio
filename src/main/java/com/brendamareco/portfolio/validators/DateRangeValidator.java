9930dea6b49630a2eff275802f5c1bb885ce904a
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-11 20:05:59 -0300

    Created a date range validator

---
+++ src/main/java/com/brendamareco/portfolio/validators/DateRangeValidator.java
@@ -0,0 +1,14 @@
+package com.brendamareco.portfolio.validators;
+
+import com.brendamareco.portfolio.embeddables.DateRange;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class DateRangeValidator implements ConstraintValidator<ValidDateRange, DateRange>
+{
+    @Override
+    public boolean isValid(DateRange value, ConstraintValidatorContext context)
+    {
+        return value.getStartDate().before(value.getEndDate());
+    }
+}

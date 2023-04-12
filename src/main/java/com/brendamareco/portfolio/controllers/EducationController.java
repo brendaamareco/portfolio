997c1278b052084589c390505c4d089d537dd8e4
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-12 00:35:27 -0300

    Updated EducationController so that it adapts to the AbstractController

--- src/main/java/com/brendamareco/portfolio/controllers/EducationController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/EducationController.java
@@ -2,43 +2,15 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.Education;
 import com.brendamareco.portfolio.services.EducationService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
-public class EducationController
+@RequestMapping("/api/education")
+public class EducationController extends AbstractController<Education, Long>
 {
-    private final String ROOT_URL = "/api/education";
-    @Autowired private EducationService educationService;
-
-    @GetMapping(ROOT_URL)
-    public List<Education> getEducationList()
-    {
-        return this.educationService.getAll();
-    }
-
-    @PostMapping(ROOT_URL)
-    public ResponseEntity<Education> add(@RequestBody @Valid Education education)
-    {
-        return ResponseEntity.ok(this.educationService.add(education));
-    }
-
-    @PutMapping(ROOT_URL)
-    public ResponseEntity<Education> update(@RequestBody @Valid Education education)
-    {
-        return ResponseEntity.ok(this.educationService.update(education));
-    }
-
-    @DeleteMapping(ROOT_URL + "/{educationId}")
-    public ResponseEntity<Education> delete(@PathVariable Long educationId)
-    {
-       this.educationService.deleteById(educationId);
-       return ResponseEntity.noContent().build();
+    EducationController(EducationService educationService) {
+        super(educationService);
     }
 
 }

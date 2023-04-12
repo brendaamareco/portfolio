968c659811beb0d6e0b87ca701bcaa482ad1c379
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-12 00:36:02 -0300

    Updated ProjectController so that it adapts to the AbstractController

--- src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/ProjectController.java
@@ -2,43 +2,15 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.Project;
 import com.brendamareco.portfolio.services.ProjectService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200/")
 @RestController
-public class ProjectController
+@RequestMapping("/api/projects")
+public class ProjectController extends AbstractController<Project, Long>
 {
-    private final String ROOT_URL = "/api/projects";
-    @Autowired private ProjectService projectService;
-
-    @GetMapping(ROOT_URL)
-    public List<Project> getAll()
-    {
-        return this.projectService.getAll();
-    }
-
-    @PostMapping(ROOT_URL)
-    public ResponseEntity<Project> add(@RequestBody @Valid Project project)
-    {
-        return ResponseEntity.ok(this.projectService.add(project));
-    }
-
-    @PutMapping(ROOT_URL)
-    public ResponseEntity<Project> update(@RequestBody @Valid Project project)
-    {
-        return ResponseEntity.ok(this.projectService.update(project));
-    }
-
-    @DeleteMapping(ROOT_URL + "/{projectId}")
-    public ResponseEntity<Project> delete(@PathVariable Long projectId)
-    {
-        this.projectService.deleteById(projectId);
-        return ResponseEntity.noContent().build();
+    public ProjectController(ProjectService projectService) {
+        super(projectService);
     }
 
 }

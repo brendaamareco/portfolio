6f6cd198e696d0bba4382959434abf8a81fd5c02
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-12 00:35:50 -0300

    Updated OwnerController so that it adapts to the AbstractController

--- src/main/java/com/brendamareco/portfolio/controllers/OwnerController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/OwnerController.java
@@ -2,27 +2,14 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.Owner;
 import com.brendamareco.portfolio.services.OwnerService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
-public class OwnerController
+@RequestMapping("/api/owner")
+public class OwnerController extends AbstractController<Owner, Long>
 {
-    private final String ROOT_URL = "/api/owner";
-    @Autowired private OwnerService ownerService;
-
-    @GetMapping(ROOT_URL)
-    public Owner get()
-    {
-        return ownerService.getAll().get(0);
-    }
-
-    @PutMapping(ROOT_URL)
-    public ResponseEntity<Owner> update(@RequestBody @Valid Owner owner)
-    {
-        return ResponseEntity.ok(this.ownerService.update(owner));
+    public OwnerController(OwnerService ownerService) {
+        super(ownerService);
     }
 }

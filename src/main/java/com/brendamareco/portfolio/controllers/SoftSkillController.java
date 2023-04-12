6533567ace27805b05a9a21b9830c077745ba14c
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-12 00:36:14 -0300

    Updated SoftSkillController so that it adapts to the AbstractController

--- src/main/java/com/brendamareco/portfolio/controllers/SoftSkillController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/SoftSkillController.java
@@ -2,32 +2,14 @@ package com.brendamareco.portfolio.controllers;
 
 import com.brendamareco.portfolio.entities.SoftSkill;
 import com.brendamareco.portfolio.services.SoftSkillService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
-public class SoftSkillController
+@RequestMapping("/api/soft-skills")
+public class SoftSkillController extends AbstractController<SoftSkill, Long>
 {
-    private final String ROOT_URL = "/api/";
-
-    @Autowired
-    private SoftSkillService softSkillService;
-
-    @GetMapping(ROOT_URL + "soft-skills")
-    public List<SoftSkill> getAll()
-    {
-        return this.softSkillService.getAll();
+    public SoftSkillController(SoftSkillService softSkillService) {
+        super(softSkillService);
     }
-
-    @PostMapping(ROOT_URL + "soft-skills")
-    public ResponseEntity<SoftSkill> add(@RequestBody @Valid SoftSkill softSkill)
-    {
-        return ResponseEntity.ok(this.softSkillService.add(softSkill));
-    }
-
 }

d656a23c0ab8b1d54004b5bd97fab7f06a30fcba
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-16 23:17:41 -0300

    Created Language controller

---
+++ src/main/java/com/brendamareco/portfolio/controllers/LanguageController.java
@@ -0,0 +1,17 @@
+package com.brendamareco.portfolio.controllers;
+
+import com.brendamareco.portfolio.entities.Language;
+import com.brendamareco.portfolio.services.LanguageService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/")
+@RequestMapping("/api/language")
+public class LanguageController extends AbstractController<Language, Long>
+{
+    LanguageController(LanguageService service) {
+        super(service);
+    }
+}

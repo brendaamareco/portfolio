f03bd7b0095be450cd0560dad5cba50304bc4440
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-16 23:12:13 -0300

    Added service annotation in HardSkillService

--- src/main/java/com/brendamareco/portfolio/services/HardSkillService.java
+++ src/main/java/com/brendamareco/portfolio/services/HardSkillService.java
@@ -2,7 +2,9 @@ package com.brendamareco.portfolio.services;
 
 import com.brendamareco.portfolio.entities.HardSkill;
 import com.brendamareco.portfolio.repositories.HardSkillRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HardSkillService extends AbstractService<HardSkill, Long>
 {
     public HardSkillService(HardSkillRepository repository){ super(repository); }

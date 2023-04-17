7f363549ceaab318915cf4be69f2154701473311
“brenda-mareco” <brendaamareco@gmail.com>
2023-04-16 23:12:54 -0300

    Created Language entity

---
+++ src/main/java/com/brendamareco/portfolio/entities/Language.java
@@ -0,0 +1,31 @@
+package com.brendamareco.portfolio.entities;
+
+import com.brendamareco.portfolio.interfaces.IEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "`language`")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Language implements IEntity<Long>
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @NotBlank
+    @Pattern(regexp = "^.{1,45}$",
+            message = "Name length must be greater than or equal to 1 and" +
+                    " less than 45")
+    String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    LanguageLevel languageLevel;
+}
---
+++ src/main/java/com/brendamareco/portfolio/entities/LanguageLevel.java
@@ -0,0 +1,3 @@
+package com.brendamareco.portfolio.entities;
+
+public enum LanguageLevel { A1, A2, B1, B2, C1, C2 }

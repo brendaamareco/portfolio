fecb8c115d52929f06032313881b241c74e1355d
“brenda-mareco” <brendaamareco@gmail.com>
2023-05-02 15:29:23 -0300

    Deleted unused method

--- src/main/java/com/brendamareco/portfolio/controllers/EducationController.java
+++ src/main/java/com/brendamareco/portfolio/controllers/EducationController.java
@@ -13,13 +13,4 @@ public class EducationController extends AbstractController<Education, Long>
     EducationController(EducationService educationService) {
         super(educationService);
     }
-
-    @CrossOrigin(origins = "http://localhost:4200")
-    @RequestMapping(value = "/myEndpoint", method = RequestMethod.OPTIONS)
-    public void handlePreflightRequest(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        response.setHeader("Access-Control-Max-Age", "3600");
-    }
 }

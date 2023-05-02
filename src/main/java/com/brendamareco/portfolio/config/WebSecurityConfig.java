6ffc91a2920d57cb86a4f60c9576eb4f5183183d
“brenda-mareco” <brendaamareco@gmail.com>
2023-05-02 15:28:03 -0300

    Deleted unused imports

--- src/main/java/com/brendamareco/portfolio/config/WebSecurityConfig.java
+++ src/main/java/com/brendamareco/portfolio/config/WebSecurityConfig.java
@@ -15,15 +15,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity

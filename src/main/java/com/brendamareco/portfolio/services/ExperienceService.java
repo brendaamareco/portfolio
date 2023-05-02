ed1d9918868e92657a49db548f24648b1117e9cd
“brenda-mareco” <brendaamareco@gmail.com>
2023-05-02 15:37:52 -0300

    Deleted unused import

--- src/main/java/com/brendamareco/portfolio/services/ExperienceService.java
+++ src/main/java/com/brendamareco/portfolio/services/ExperienceService.java
@@ -2,7 +2,6 @@ package com.brendamareco.portfolio.services;
 
 import com.brendamareco.portfolio.entities.Experience;
 import com.brendamareco.portfolio.repositories.ExperienceRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
